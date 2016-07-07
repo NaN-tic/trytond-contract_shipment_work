a9f2f6bab210d8c04f8c74f07cd28904d57b9d30
??ngel ??lvarez <angel@nan-tic.com>
2016-07-07 10:30:02 +0200

    add relations to shipment_work, and calc cost and revenue for contract_line origin

--- contract.py
+++ contract.py
@@ -4,6 +4,8 @@ import datetime
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import rrule
 from sql.aggregate import Max
+from sql import Cast, Literal
+from sql.functions import Substring, Position
 
 from trytond.model import fields, ModelView
 from trytond.pool import Pool, PoolMeta
@@ -41,28 +43,22 @@ class ContractService(RRuleMixin):
 class ShipmentWork:
     __name__ = 'shipment.work'
 
-    # TODO: Maybe origin could be better.
-    contract_line = fields.Many2One('contract.line', 'Contract Line',
-        select=True)
+    contract = fields.Function(fields.Many2One('contract', 'Contract'),
+        'get_contract', searcher='search_contract')
+
+    def get_contract(self, name):
+        return (self.origin and self.origin.contract and
+            self.origin.contract.id or None)
 
     @classmethod
-    def __setup__(cls):
-        pool = Pool()
-        Asset = None
-        try:
-            Asset = pool.get('asset')
-        except KeyError:
-            pass
-        if Asset:
-            # Register asset before __setup__ so on_change are not cleared
-            cls.asset = fields.Many2One('asset', 'Asset',
-                domain=[
-                    If(Bool(Eval('party')),
-                        [('owners.owner', '=', Eval('party'))],
-                        []),
-                    ],
-                depends=['party'])
-        super(ShipmentWork, cls).__setup__()
+    def search_contract(cls, name, clause):
+        print [('origin.contract',) + tuple(clause[1:])]
+        return [('origin.contract',) + tuple(clause[1:])]
+
+    @classmethod
+    def _get_origin(cls):
+        res = super(ShipmentWork, cls)._get_origin()
+        return res + ['contract.line']
 
     @fields.depends('asset', 'employees')
     def on_change_asset(self):
@@ -86,6 +82,59 @@ class ShipmentWorkProduct:
             line.asset = self.shipment.asset
         return line
 
+class Contract:
+    __name__ = 'contract'
+
+    shipment_works = fields.Function(fields.One2Many(
+        'shipment.work', None, 'Shipment Works'), 'get_shipment_works')
+
+    @classmethod
+    def get_shipment_works(cls, contracts, name):
+        pool = Pool()
+        ContractLine = pool.get('contract.line')
+        ShipmentWork = pool.get('contract.shipment.work')
+
+        contract_line = ContractLine.__table__()
+        shipment_work = ShipmentWork.__table__()
+
+        cursor = Transaction().connection.cursor()
+        table = cls.__table__()
+        contract_ids = [x.id for x in contracts]
+        res = dict().fromkeys(contract_ids, [])
+
+        origin_id = Cast(Substring(shipment_work.origin,
+            Position(',', shipment_work.origin) +
+            Literal(1)), cls.id.sql_type().base)
+        origin_model = Substring(shipment_work.origin,
+            0, Position(',', shipment_work.origin))
+
+        query = table.join(contract_line, 'LEFT',
+            condition=table.id == contract_line.contract
+            ).join(shipment_work, 'LEFT',
+                condition=((contract_line.id == origin_id) &
+                    (origin_model == 'contract.line'))
+            ).select(table.id, shipment_work.id,
+                where=reduce_ids(table.id, contract_ids),
+            )
+
+        cursor.execute(*query)
+        for contract, shipment_work in cursor.fetchall():
+            res[contract].append(shipment_work)
+        return res
+
+        @classmethod
+        def get_cost_and_revenue(cls, contracts, names):
+            res = super(Contract, cls).get_cost_and_revenue(contracts, names)
+            for contract in contracts:
+                for shipment in contract.shipment_works:
+                    if shipment.state != 'done':
+                        continue
+                    if 'cost' in names:
+                        res['cost'][contract.id] += shipment.cost
+                    if 'revenue' in names:
+                        res['revenue'][contract.id] += shipment.cost
+            return res
+
 
 class ContractLine:
     __name__ = 'contract.line'
@@ -99,6 +148,22 @@ class ContractLine:
             'required': Bool(Eval('create_shipment_work')),
             'invisible': ~Bool(Eval('create_shipment_work')),
             }, depends=['create_shipment_work'])
+    shipment_works = fields.One2Many('shipment.work', 'origin',
+        'ShipmentWork')
+
+    @classmethod
+    def get_cost_and_revenue(cls, lines, names):
+        res = super(ContractLine, cls).get_cost_and_revenue(lines, names)
+
+        for line in lines:
+            for shipment in line.shipment_works:
+                if shipment.state != 'done':
+                    continue
+                if 'cost' in names:
+                    res['cost'][line.id] += shipment.cost
+                if 'revenue' in names:
+                    res['revenue'][line.id] += shipment.cost
+        return res
 
     @classmethod
     def get_last_work_shipment_date(cls, lines, name):
@@ -108,10 +173,13 @@ class ContractLine:
         cursor = Transaction().connection.cursor()
         line_ids = [l.id for l in lines]
         values = dict.fromkeys(line_ids, None)
-        cursor.execute(*table.select(table.contract_line,
+        cursor.execute(*table.select(table.origin,
                     Max(table.planned_date),
-                where=reduce_ids(table.contract_line, line_ids),
-                group_by=table.contract_line))
+                where=reduce_ids(
+                    Cast(Substring(table.origin, Position(',', table.origin) +
+                        Literal(1)), cls.id.sql_type().base),
+                    line_ids),
+                group_by=table.origin))
         values.update(cursor.fetchall())
         return values
 
@@ -138,7 +206,7 @@ class ContractLine:
         shipment = ShipmentWork()
         shipment.party = self.contract.party
         shipment.planned_date = planned_date
-        shipment.contract_line = self
+        shipment.origin = self
         shipment.work_description = self.service.work_description
         if self.contract.party.customer_payment_term:
             shipment.payment_term = self.contract.party.customer_payment_term
@@ -158,6 +226,11 @@ class ContractLine:
 
         shipment_works = cls.get_shipment_works(lines, date)
         return ShipmentWork.create([w._save_values for w in shipment_works])
+
+    @classmethod
+    def get_totals(cls, lines, names):
+        res = super(ContractLine, cls).get_totals(lines, names)
+
 
 
 class CreateShipmentsStart(ModelView):
